08c942fb347181f8e839ed2c2883862671779a96
Alexandr <das20052006@gmail.com>
2023-03-12 18:18:03 +0300

    add new configurations

--- backend/DemoInternetShop.ProductsAPI/Program.cs
+++ backend/DemoInternetShop.ProductsAPI/Program.cs
@@ -8,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 var app = builder.Build();
 
@@ -15,18 +20,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = "api";
+    });
 }
+app.UseForwardedHeaders();
 
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
-
-//app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("api/products", async () =>
+app.MapGet("/products", async () =>
 {
     using (var client = new HttpClient())
     {
